78b91d70864b394de2964367d6d6aa1a730b8ff7
abbiehuynh <abbiehuynhh@gmail.com>
2024-07-09 03:55:41 -0500

    Submitting JS Challenges

--- prework/7_FunctionsChallenges.js
+++ prework/7_FunctionsChallenges.js
@@ -15,6 +15,12 @@
 // prependToString('awesome', 'very') // --> 'veryawesome'
 // prependToString('world', 'hello ') // --> 'hello world'
 // prependToString('nothing', '') // --> 'nothing'
+    function prependToString (str1, str2) {
+        return str2 + str1;
+    }
+    // console.log(prependToString('awesome', 'very'));
+    // console.log(prependToString('world', 'hello '));
+    // console.log(prependToString('nothing', ''));
 
 // Exercise 2. Write a function called stringIncludes, which accepts two strings: the first string is a word and the second string is a single character.
 // The function should return true if the first string includes the character, otherwise it should return false.
@@ -22,6 +28,21 @@
 // Examples:
 // stringIncludes('awesome', 'e'); // --> true
 // stringIncludes('awesome', 'z'); // --> false
+    function stringIncludes (str3, str4) {
+        if (str4 === '') {
+            return false;
+        } else if (str3.indexOf(str4) > -1) {
+            return true;
+        } else {
+            return false;
+       }
+    }
+    // console.log(stringIncludes('awesome', 'e')); // true
+    // console.log(stringIncludes('awesome', 'z')); // false
+    // console.log(stringIncludes('hello', 'o')); // true
+    // console.log(stringIncludes('yes', '4')); // false
+    // console.log(stringIncludes('yes', 4)); // false
+    // console.log(stringIncludes('yes', '')); // false
 
 // Exercise 3. Write a function called stringLastIndexOf, which accepts two strings: the first is a word and the second is a single character.
 // The function should return the last index at which the character exists or -1 if the character is not found.
@@ -29,6 +50,20 @@
 // Examples:
 // stringLastIndexOf('awesome', 'e'); // --> 6
 // stringLastIndexOf('awesome', 'z'); // --> -1
+    function stringLastIndexOf (str5, str6) {
+        for (let i = str5.length - 1; i >= 0; i--) {
+            if (str5[i] == str6) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // console.log(stringLastIndexOf('awesome', 'e')); // 6
+    // console.log(stringLastIndexOf('awesome', 'z')); // -1
+    // console.log(stringLastIndexOf('awesome', 'm')); // 5
+    // console.log(stringLastIndexOf('mummy', 'm')); // 3
+    // console.log(stringLastIndexOf('mummy', '')); // -1
+    // console.log(stringLastIndexOf('mummy', 0)); // -1
 
 // Exercise 4. Write a function called removeFromString, which accepts a string, a starting index (number) and a number of characters to remove.
 // The function should return a new string with the characters removed.
@@ -38,20 +73,50 @@
 // removeFromString('Hello School', 0, 6) // --> 'School'
 // removeFromString('Hello School', 2, 4) // --> 'HeSchool'
 // removeFromString('Hello School', 6, 400) // --> 'Hello '
+    function removeFromString (str7, stIndex, chToRemove) {
+        let newString = "";
+        newString = str7.slice(0, stIndex) + str7.slice(stIndex + chToRemove);
+        return newString;
+    }
+    // console.log(removeFromString('Elie', 2, 2)) // --> 'El'
+    // console.log(removeFromString('Elie', 0, 1)) // --> 'lie'
+    // console.log(removeFromString('Hello School', 0, 6)) // --> 'School'
+    // console.log(removeFromString('Hello School', 2, 4)) // --> 'HeSchool'
+    // console.log(removeFromString('Hello School', 6, 400)) // --> 'Hello '
 
 // Exercise 5. Write a function called indexOf, which accepts an array and a number.
 // The function should return the first index at which the value exists or -1 if the value is not found.
 // IMPORTANT: Do not use the built in array.indexOf() function!
 // Examples:
-// let arr = [5, 10, 15, 20];
+    let arr = [5, 10, 15, 20];
 // indexOf(arr, 20); // --> 3
-// let arr2 = [1, 2, 3, 4, 5];
+    let arr2 = [1, 2, 3, 4, 5];
 // indexOf(arr2, 2); // --> 1
-// let arr3 = [1, 2];
+    let arr3 = [1, 2];
 // indexOf(arr3, 10); // --> -1
+    let arr4 = [3, 4, 3];
+    function indexOf (arr, num) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // console.log(indexOf(arr,20)); // 3
+    // console.log(indexOf(arr,5)); // 0
+    // console.log(indexOf(arr,18)); // -1
+    // console.log(indexOf(arr2,2)); // 1
+    // console.log(indexOf(arr3, 10)); // -1
+    // console.log(indexOf(arr4, 3)); // 0
 
-// Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
-// The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
+// Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. 
+// The function should return true if the value exists in the collection when we search starting from the starting index. 
+// Otherwise, it should return false.
+// The collection can be a string, an array, or an object. 
+    // If the collection is a string or array, the third parameter is a starting index for where to search from. 
+    // If the collection is an object, the function searches for the value among values in the object; 
+        // since objects have no sort order, the third parameter is ignored.
 // Examples:
 // includes([1, 2, 3], 1) // --> true
 // includes([1, 2, 3], 1, 2) // --> false
@@ -61,3 +126,32 @@
 // includes('abcd', 'b') // --> true
 // includes('abcd', 'e') // --> false
 // includes('abcd', 'a', 2) // --> false
+
+// to find val in collection, begin search at starting index
+    // if there is a val in collection, start
+
+    function includes (collection, val, stIndex2) {
+        if (collection instanceof Array) {
+            if (collection.indexOf(val,stIndex2) > -1) {
+                return true;
+            }
+        } else if (typeof collection === "string") {
+            if (collection.indexOf(val,stIndex2) > -1) {
+                return true;
+            }
+        }  else if (typeof collection === "object") {
+            if (Object.values(collection).indexOf(val) > -1) {
+                return true;
+            }
+        }
+        return false;
+        }
+    // console.log(includes([1, 2, 3], 1)); // true 
+    // console.log(includes([1, 2, 3], 1, 2)); // --> false
+    // console.log(includes([1, 2, 3], 6)); // --> false
+    // console.log(includes({ 'a': 1, 'b': 2 }, 1)); // --> true 
+    // console.log(includes({ 'a': 1, 'b': 2 }, 'a')); // --> false 
+    // console.log(includes('abcd', 'b')); // --> true
+    // console.log(includes('abcd', 'e')); // --> false
+    // console.log(includes('abcd', 'a', 2)); // --> false
+    
--- prework/8_ObjectsChallenges.js
+++ prework/8_ObjectsChallenges.js
@@ -12,22 +12,44 @@
 // Exercise 1. Write a function called keys, which accepts an object and returns an array of all of the keys in the object.
 // IMPORTANT: Do not use the built in Object.keys() function!
 // Examples:
-// let obj = { a: 1, b: 2, c: 3 };
+    let obj = { a: 1, b: 2, c: 3 };
 // keys(obj); // ["a", "b", "c"]
-// let obj2 = { first: 'Matt', last: 'Lane' };
+    let obj2 = { first: 'Matt', last: 'Lane' };
 // keys(obj2); // ["first", "last"]
-// let obj3 = {};
+    let obj3 = {};
 // keys(obj3); // []
+    function keys (obj) {
+        let arrOfObj = Object.entries(obj);
+        let arrOfKeys = [];
+        for (let i = 0; i < arrOfObj.length; i++) {
+            arrOfKeys.push(Object.entries(obj)[i][0]);
+        }
+        return arrOfKeys;
+    }
+    // console.log(keys(obj)); // ["a", "b", "c"]
+    // console.log(keys(obj2)); // ["first", "last"]
+    // console.log(keys(obj3)); // []
 
 // Exercise 2. Write a function called values, which accepts an object and returns an array of all of the values in the object.
 // IMPORTANT: Do not use the built in Object.values() function!
 // Examples:
 // let obj = { a: 1, b: 2, c: 3 };
 // values(obj); // [1,2,3]
-// let obj2 = { first: 'Matt', last: 'Lane', isDogOwner: true };
+    let obj4 = { first: 'Matt', last: 'Lane', isDogOwner: true };
 // values(obj2); // ["Matt", "Lane", true]
 // let obj3 = {};
 // values(obj3); // []
+    function values (obj1) {
+        let arrOfObj2 = Object.entries(obj1);
+        let arrOfValues = [];
+        for (let i = 0; i < arrOfObj2.length; i++) {
+            arrOfValues.push(Object.entries(obj1)[i][0,1]);
+        }
+        return arrOfValues;
+    }
+    // console.log(values(obj)); // [1,2,3]
+    // console.log(values(obj4)); // ["Matt", "Lane", true]
+    // console.log(values(obj3)); // []
 
 // Exercise 3. Write a function called entries, which accepts an object and returns an array of arrays of key-value pairs.
 // In other words, each sub-array is an "entry" in the object with two elements: the first element is the key, and the second element is the value.
@@ -41,6 +63,14 @@
 // [["first","Matt"], ["last","Lane"], ["isDogOwner",true]]
 // let obj3 = {};
 // entries(obj3); // []
+    function entries (obj5) {
+        return Object.keys(obj5).map(function(key) {
+            return [key, obj5[key]];
+        });
+    }
+    // console.log(entries(obj)); // [["a",1], ["b",2], ["c",3]]
+    // console.log(entries(obj4)); // [["first","Matt"], ["last","Lane"], ["isDogOwner",true]]
+    // console.log(entries(obj3)); // []
 
 // Exercise 4. Write a function called pluck, which takes an array of objects and the name of a key.
 // The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
@@ -49,6 +79,16 @@
 // ["Tim", "Matt", "Elie"]
 // pluck([{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],'isBoatOwner')
 // [true, false, undefined]
+    function pluck (obj6, key) {
+        let arr2 = [];
+        for (let i = 0; i < obj6.length; i++) {
+            arr2.push(obj6[i][key]);
+        }
+        return arr2;
+    }
+    // console.log(pluck([{ name: "Tim" }, { name: "Matt" }, { name: "Elie" }],'name')); // ["Tim", "Matt", "Elie"]
+    // console.log(pluck([{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],'isBoatOwner')); 
+        // [true, false, undefined]
 
 // Exercise 5. Write a function called stringFromObject that generates a string from an object's key/value pairs.
 // The format should be "key = value, key = value".
@@ -59,6 +99,12 @@
 // stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false });
 // "name = Elie, job = Instructor, isCatOwner = false"
 // stringFromObject({}); // ""
+    function stringFromObject (obj7) {
+       return Object.entries(obj7).map(([key,val]) => `${key} = ${val}`).join(', ');
+    }
+    // console.log(stringFromObject({ a: 1, b: '2' })); // "a = 1, b = 2"
+    // console.log(stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false })); // "name = Elie, job = Instructor, isCatOwner = false"
+    // console.log(stringFromObject({})); // ""
 
 // Exercise 6. Write a function called minMaxKeyInObject that accepts an object with numeric keys.*
 // The function should return an array with the following format: [lowestKey, highestKey]
@@ -67,3 +113,12 @@
 // [1, 10]
 // minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' });
 // [1, 4]
+    function minMaxKeyInObject (obj8) {
+        let arr3 = Object.keys(obj8);
+        let lowestKey = Math.min(...arr3);
+        let highestKey = Math.max(...arr3);
+        let keyValues = [lowestKey, highestKey];
+        return keyValues;
+    }
+    // console.log(minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' })); // [1, 10]
+    // console.log(minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' })); // [1, 4]
